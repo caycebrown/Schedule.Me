f1e0dd4084f675948b5bcd7202c05e7dc9ef42a3
cayce brown <caycebrown98@gmail.com>
2022-04-24 01:23:58 -0500

    added route for serving static react app in production env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +12,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/test', (req, res) => {
     res.send({message: 'Is anybody out there?'})
 })
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "client/build")))
+}
 
 app.listen(port, () => {
 console.log(`app is listening on ${port}`)
