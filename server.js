7826ab1ae62808d0b67c440a3754c91767d94fb4
cayce brown <caycebrown98@gmail.com>
2022-04-24 01:41:08 -0500

    added eslint config and updated eslint version

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
-const app = express();
-const path = require("path");
+const app = express()
+const path = require('path')
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-//TEST API ROUTE FOR DEVELOPMENT ONLY
+// TEST API ROUTE FOR DEVELOPMENT ONLY
 app.get('/api/test', (req, res) => {
-    res.send({message: 'Is anybody out there?'})
+  res.send({ message: 'Is anybody out there?' })
 })
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "client/build")))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
 }
 
 app.listen(port, () => {
-console.log(`app is listening on ${port}`)
+  console.log(`app is listening on ${port}`)
 })
